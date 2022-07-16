5b25802d92065c4521dc55b6305fa5a691242404
davehernandez92 <davehgal@gmail.com>
2022-07-15 23:40:18 -0300

    Test CDN

---
+++ app.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    VANTA.NET({
+        el: "#container",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00
+      })
+})
